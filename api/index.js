5917c882d56d01da02a914eddafe6983bd6156be
HellemansKoen <78733780+HellemansKoen@users.noreply.github.com>
2021-11-15 14:04:27 +0100

    test
    ik heb iets getest, niets van aantrekken

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 app.use(express.json());
 app.get('/', (req, res) => {
-    res.json('Hello world!');
+    res.json('Hello world');
 });
 app.listen(3000, () => {
     console.log('Started api on http://localhost:3000');
