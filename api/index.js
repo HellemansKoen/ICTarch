728b2b75f5854679b015ef5e37b3e3e15bac34ab
HellemansKoen <78733780+HellemansKoen@users.noreply.github.com>
2021-11-15 14:33:23 +0100

    security

--- api/index.js
+++ api/index.js
@@ -1,9 +1,23 @@
+const loginfo = require('');
+const
 const express = require('express')
 const app = express();
 app.use(express.json());
+var security = require('./security');
+security.validateToken('test');
+
 app.get('/', (req, res) => {
     res.json('Hello world');
 });
+
+app.get('/post', (req, res) => {
+
+});
+
+app.get('/login', (req, res) => {
+
+});
+
 app.listen(3000, () => {
     console.log('Started api on http://localhost:3000');
 })
