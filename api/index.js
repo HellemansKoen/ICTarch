cb56d206b0e90bc9e6fd256e1c29c8265065ee6e
HellemansKoen <78733780+HellemansKoen@users.noreply.github.com>
2021-11-22 15:42:18 +0100

    inloggen+ registreren --> werkt

--- api/index.js
+++ api/index.js
@@ -1,22 +1,28 @@
-const loginfo = require('');
-const
 const express = require('express')
+const bodyParser = require("body-parser");
 const app = express();
 app.use(express.json());
+app.use(bodyParser.json());
+
 var security = require('./security');
-security.validateToken('test');
 
 app.get('/', (req, res) => {
     res.json('Hello world');
 });
 
-app.get('/post', (req, res) => {
-
+app.post('/register', (req, res) => {
+    security.registerUser(req.body.email, req.body.password)
+        .then((cogResponse) => res.status(201).json(cogResponse))
+        .catch((cogResponse) => res.status(204).json(cogResponse))
 });
 
-app.get('/login', (req, res) => {
-
+app.post('/login', (req, res) => {
+    security.login(req.body.email, req.body.password)
+        .then((cogResponse) => res.status(201).json(cogResponse))
+        .catch((cogResponse) => res.status(204).json(cogResponse))
 });
+app.post("/upload", (req, res) => {});
+
 
 app.listen(3000, () => {
     console.log('Started api on http://localhost:3000');
