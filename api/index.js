9e8add32724fa4a2d119bfc213a57cd558548b52
HellemansKoen <78733780+HellemansKoen@users.noreply.github.com>
2021-11-23 22:01:21 +0100

    uploaden

--- api/index.js
+++ api/index.js
@@ -1,51 +1,49 @@
 const express = require('express')
 const bodyParser = require("body-parser");
+const AWS = require('aws-sdk');
+const fileUpload = require("express-fileupload")
+const Region = "uw-east-1";
+const { S3Client } = require("@aws-sdk/client-s3");
+const { PutObjectCommand } = require("@aws-sdk/client-s3");
+const s3 = new S3Client({ region: Region })
 const app = express();
+const security = require('./security');
+app.use(fileUpload())
 app.use(express.json());
 app.use(bodyParser.json());
-
-
-var security = require('./security');
-
+// Homepage
 app.get('/', (req, res) => {
     res.json('Hello world');
 });
-
+// Registreren
 app.post('/register', (req, res) => {
     security.registerUser(req.body.email, req.body.password)
         .then((cogResponse) => res.status(201).json(cogResponse))
-        .catch((cogResponse) => res.status(204).json(cogResponse))
+        .catch((cogResponse) => res.status(404).json(cogResponse))
 });
-
+// login
 app.post('/login', (req, res) => {
     security.login(req.body.email, req.body.password)
         .then((cogResponse) => res.status(201).json(cogResponse))
-        .catch((cogResponse) => res.status(204).json(cogResponse))
+        .catch((cogResponse) => res.status(404).json(cogResponse))
 });
+// luister naar poort
 app.listen(3000, () => {
     console.log('Started api on http://localhost:3000');
-})
-app.post("/downloaden/:nummer", (req, res) => {});
-// nieuw --> proberen uploaden
-app.post("/upload", (req, res) => {
-    uploadFile(req.params.file);
 });
-const fs = require('fs');
-const AWS = require('aws-sdk');
+// Downloaden bestanden --> nog doen
+app.get("/api/files/{uuid} ", (req, res) => {});
+// Uploaden bestanden ==> vragen voor na te kijken
+app.post("/uploaden", (req, res) => {
+    uploadFile(req.files.bestand);
+});
 const uploadFile = (bestand) => {
-    // Read content from the file
-    const fileContent = fs.readFileSync(bestand);
     // Setting up S3 upload parameters
     const params = {
-        Bucket: buckets3project,
-        Key: bestand.fileName, // File name you want to save as in S3
-        Body: fileContent
+        Bucket: "buckets3project",
+        Key: bestand.name, // File name you want to save as in S3
+        Body: bestand.data
     };
     // Uploading files to the bucket
-    s3.upload(params, function(err, data) {
-        if (err) {
-            throw err;
-        }
-        console.log(`File uploaded successfully. ${data.Location}`);
-    });
+    s3.send(new PutObjectCommand(params));
 }
